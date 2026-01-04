f590af6ada2683081e616c593142283d29881000
DaniloFeeburg <danilofeeburg@gmail.com>
2026-01-04 00:09:05 -0300

    chore: enlarge logo and favicon sizes

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -65,7 +65,7 @@ export const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) =>
             <img
               src="/fincontrol_logo_transparente.png"
               alt="FinControl"
-              className="h-8 w-auto"
+              className="h-12 w-auto"
             />
           </div>
 
@@ -162,7 +162,7 @@ export const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) =>
           <img
             src="/fincontrol_logo_transparente.png"
             alt="FinControl"
-            className="h-6 w-auto"
+            className="h-8 w-auto"
           />
           <button 
             onClick={() => setIsOpen(!isOpen)}
